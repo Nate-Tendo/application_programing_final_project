ffe4e291ced24609c9a0fba6948b229372acaffc
Nate-Tendo <nathan.lee8032@gmail.com>
2025-11-08 02:00:14 -0600

    keeping classes seperate from main

--- classes.py
+++ classes.py
@@ -1,7 +1,9 @@
 import numpy as np
 from typing import List
 
-G = 6.67430e-11  # gravitational constant
+
+# GRAVITY_CONSTANT = 6.67430e-11  # gravitational constant --> Although this is the true G, we need to scale it for realism
+GRAVITY_CONSTANT = 1
 
 class Vector2:
     """2D vector class backed by numpy for fast vector math."""
@@ -15,20 +17,35 @@ class Vector2:
     @property
     def y(self):
         return self.v[1]
-
+    
+    @property
+    def angle(self):
+        return np.atan2(self.y,self.x)
+    
     def __add__(self, other):
         return Vector2(*(self.v + other.v))
+    
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        return self.__add__(other)
 
     def __sub__(self, other):
         return Vector2(*(self.v - other.v))
+    
+    # TODO
+    # Def __rsub__
 
     def __mul__(self, scalar: float):
         return Vector2(*(self.v * scalar))
+    
+    def __rmul__(self, scalar):
+        return self.__mul__(scalar)  # handles float * Vector2
 
     def magnitude(self):
         return np.linalg.norm(self.v)
 
-    def normalized(self):
+    def normalized(self):  # Could rename to be 'unit_vect?"
         mag = self.magnitude()
         return Vector2(*(self.v / mag)) if mag > 0 else Vector2(0, 0)
 
@@ -36,35 +53,63 @@ class Vector2:
         return Vector2(self.x, self.y)
 
     def __repr__(self):
-        return f"Vector2({self.x:.3f}, {self.y:.3f})"
+        return f"Vector2({self.x:.6f}, {self.y:.6f})" # Needed more decimals for a smaller time step change
 
 class Body:
     """Base class for planets, moons, stars, spacecraft."""
     _instances = []
-    def __init__(self, name: str, mass: float, position: Vector2, velocity: Vector2):
+    
+    # TODO: Make it easier to initialize velocity at 0 (automatically convert 0 -> Vector2(0,0))
+    
+    def __init__(self, name: str, mass: float, position: Vector2, velocity: Vector2, color = 'blue'):
         self.name = name
         self.mass = mass # could add a density and size alternative instead of just mass
         self.position = position
         self.velocity = velocity
+        self.color = color
         Body._instances.append(self)
 
     def gravitational_acceleration_from(self, other: "Body") -> Vector2:
         # Newton’s law of gravitation
         direction = other.position - self.position
         distance = direction.magnitude()
-        force_mag = G * other.mass / (distance**2)
+        force_mag = GRAVITY_CONSTANT * other.mass / (distance**2)
         return direction.normalized() * force_mag
+    
+    # TODO, define the following
+    # def plot(self): # Plot a planet
+    #     plt.plot(self.x,self.y,'.',markersize=self.m)
+    # def pos(self):
+    #     return vector(self.x,self.y)
+    # def vectorfield(self): # Each body would create a vector field. In another visualization, we would sum from all of the bodies.
+    #     return(X,Y,U,V)
 
 class Spacecraft(Body):
-    def __init__(self, name, mass, position, velocity, thrust=0.0, orientation=0.0):
-        super().__init__(name, mass, position, velocity)
+    def __init__(self, name, mass, position, velocity, color, thrust=0.0, orientation=0.0):
+        super().__init__(name, mass, position, velocity, color)
         self.thrust = thrust
         self.orientation = orientation  # radians
+        self.path = [self.position.copy()]
 
     def propulsion_acceleration(self):
         ax = self.thrust * np.cos(self.orientation) / self.mass
         ay = self.thrust * np.sin(self.orientation) / self.mass
         return Vector2(ax, ay)
+    
+    def compute_total_current_force(self):
+        return sum([self.gravitational_acceleration_from(body) for body in Body._instances if not isinstance(body,Spacecraft)])
+    
+    def step_forward_dt(self, time_step = 0.1):
+        # Use Velocity Verlet Numerical Integration
+        total_force = self.compute_total_current_force()
+        new_position = self.position + self.velocity*time_step + (1/2)*total_force*(time_step**2)
+        self.position = new_position
+        new_total_force_n_plus_1 = self.compute_total_current_force()
+        new_velocity = self.velocity + (1/2)*(total_force + new_total_force_n_plus_1)*time_step
+        self.velocity = new_velocity
+        self.path.append(self.position.copy())
+        return
+    
 class SolarSystem:
     """A simple solar system simulator."""
     def __init__(self, name: str, star: Body):
